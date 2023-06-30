15ee0d844b35525ca6b561ebbd9a7f38cbbf799f
LinuxBeaver <78667207+LinuxBeaver@users.noreply.github.com>
2023-06-30 22:33:58 +0000

    Add files via upload

--- compile_gegl_metallic_here/metal.c
+++ compile_gegl_metallic_here/metal.c
@@ -17,6 +17,14 @@
  * 2022 Beaver (GEGL metallic)
  */
 
+/*
+June 25 2023- Recreation of Metallic's GEGL Graph.
+alien-map cpn-1-frequency=6 cpn-2-frequency=5.9 cpn-3-frequency=3
+hue-chroma lightness=-5
+noise-reduction iterations=3
+saturation scale=0
+ */
+
 #include "config.h"
 #include <glib/gi18n-lib.h>
 
@@ -30,6 +38,7 @@ property_double (solar1, _("Solarization of Red Channel"), 2.7)
                         " color-model {hsl} : hsl-label]")
   ui_meta     ("rgb-label", _("Red frequency"))
   ui_meta     ("hsl-label", _("Hue frequency"))
+  ui_steps      (0.100, 1.00)
 
 property_double  (solar2, _("Solarization of Green Channel"), 2.8)
   value_range (2.2, 4)
@@ -38,6 +47,7 @@ property_double  (solar2, _("Solarization of Green Channel"), 2.8)
                         " color-model {hsl} : hsl-label]")
   ui_meta     ("rgb-label", _("Green frequency"))
   ui_meta     ("hsl-label", _("Saturation frequency"))
+  ui_steps      (0.100, 1.00)
 
 property_double  (solar3, _("Solarization of Blue Channel"), 2.1)
   value_range (0, 4)
@@ -46,32 +56,24 @@ property_double  (solar3, _("Solarization of Blue Channel"), 2.1)
                         " color-model {hsl} : hsl-label]")
   ui_meta     ("rgb-label", _("Blue frequency"))
   ui_meta     ("hsl-label", _("Lightness frequency"))
-
-
+  ui_steps      (0.100, 1.00)
 
 
 property_double (light, _("Lightness"), 0.0)
    description  (_("Lightness adjustment"))
    value_range  (-10.0, 10.0)
+  ui_steps      (0.200, 1.00)
 
 property_int  (smooth, _("Smoothness"), 2)
   description (_("Smoothness"))
   value_range (0, 8)
   ui_range    (0, 8)
 
-property_double (desaturate, _("Desaturation Channel Lock"), 0.0)
-    description(_("Filter has no options but is needed"))
-    value_range (0.0, 0.0)
-    ui_range (0.0, 0.0)
-    ui_meta     ("role", "output-extent")
-
 property_double (value, _("Slide to invert colors on original image"), 0)
     description (_("Invert with Slider"))
     value_range (0, 1)
-    ui_gamma   (90)
+  ui_steps      (1.00, 1.00)
 	
-
-
 
 #else
 
@@ -84,7 +86,7 @@ property_double (value, _("Slide to invert colors on original image"), 0)
 static void attach (GeglOperation *operation)
 {
   GeglNode *gegl = operation->node;
-  GeglNode *input, *output, *solar, *desat, *smooth, *light, *invert, *opacity, *over, *ocolor;
+  GeglNode *input, *output, *solar, *desat, *smooth, *light, *invert, *opacity, *over;
 
   input    = gegl_node_get_input_proxy (gegl, "input");
   output   = gegl_node_get_output_proxy (gegl, "output");
@@ -96,7 +98,7 @@ static void attach (GeglOperation *operation)
 
 
   desat = gegl_node_new_child (gegl,
-                                  "operation", "gegl:saturation",
+                                  "operation", "gegl:saturation", "scale", 0.0,
                                   NULL);
 
 
@@ -110,12 +112,7 @@ static void attach (GeglOperation *operation)
                                   NULL);
 
 
-
-   ocolor = gegl_node_new_child (gegl,
-                                  "operation", "gimp:colorize",
-                                  NULL);
-
-   opacity = gegl_node_new_child (gegl,
+  opacity = gegl_node_new_child (gegl,
                                   "operation", "gegl:opacity",
                                   NULL);
 
@@ -127,32 +124,17 @@ static void attach (GeglOperation *operation)
                                   "operation", "gegl:over",
                                   NULL);
 
-
   gegl_operation_meta_redirect (operation, "solar1", solar, "cpn-1-frequency");
-
   gegl_operation_meta_redirect (operation, "solar2", solar, "cpn-2-frequency");
-
   gegl_operation_meta_redirect (operation, "solar3", solar, "cpn-3-frequency");
-
-  gegl_operation_meta_redirect (operation, "desaturate", desat, "scale");
-
   gegl_operation_meta_redirect (operation, "light", light, "lightness");
-
   gegl_operation_meta_redirect (operation, "smooth", smooth, "iterations");
-
   gegl_operation_meta_redirect (operation, "value", opacity, "value");
-
-
-
-
 
 
   gegl_node_link_many (input, over, solar, desat, smooth, light, output, NULL);
   gegl_node_connect_from (over, "aux", opacity, "output");
   gegl_node_link_many (input, invert, opacity, NULL);
-
-
-
 
 }
 
@@ -167,10 +149,10 @@ gegl_op_class_init (GeglOpClass *klass)
 
   gegl_operation_class_set_keys (operation_class,
     "name",        "gegl:metallic",
-    "title",       _("Add a metal effect to subjects"),
+    "title",       _("Metal Effect"),
     "categories",  "Aristic",
     "reference-hash", "45ed5656h28ff20fjf25sb2ac",
-    "description", _("Metalize people with GEGL (This filter works best on human skin).  "
+    "description", _("Metalize subjects and objects with GEGL (This filter works best on human skin).  "
                      ""),
     NULL);
 }
